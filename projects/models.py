939187ef0722416d1b23480788bbe567c9f33bcc
Anthony Longo <ferrari13@me.com>
2020-03-30 23:04:59 -0400

    created admin privleges for project creation

--- projects/models.py
+++ projects/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+import os
 
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/img")
+    image = models.FilePathField(path=f"{os.getcwd()}/projects/static/img")
